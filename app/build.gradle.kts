3f64955128c7b059b2fc45b96b1dd23c911ab5e0
relt <haxrelt@gmail.com>
2023-12-18 15:03:58 +0300

    switch to api34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ru.evteev.volkwagen"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ru.evteev.volkwagen"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
